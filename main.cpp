4eed65bb99e525bfdafb16d4bff9c4b9af9ca303
NAKAJIMA Yusaku <yupong7@gmail.com>
2024-07-18 10:13:27 +0900

    read by line

--- main.cpp
+++ main.cpp
@@ -18,25 +18,19 @@ auto read_by_char(Command &cmd) {
         }
         cmd.output += c;
     }
-
 }
 
-auto read_by_line() {
-    int line = 0;
-    std::string output{};
-
+auto read_by_line(Command &cmd) {
     std::string s;
     while(std::getline(std::cin, s)) {
-        line++;
-        output += s + "\n"s;
+        cmd.line++;
+        cmd.output += s + "\n"s;
     }
-
-    std::cout << std::format("line: {}, characters: {}\n", line, output.size());
 }
 
 
 int main() {
     Command c;
-    read_by_char(c);
+    read_by_line(c);
     std::cout << std::format("line: {}, characters: {}\n", c.line, c.output.size());
 }
